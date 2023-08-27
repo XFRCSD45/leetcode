e0713ed56018abc5e8079cffe6747a50b110b231
Sunny Das <108056620+XFRCSD45@users.noreply.github.com>
2023-08-27 18:23:56 +0530

    Time: 229 ms (37.95%), Space: 56.3 MB (39.82%) - LeetHub

---
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -0,0 +1,105 @@
+// class Solution {
+// public:
+//     bool solve(vector<int>&stones, int ind,int k,unordered_map<int,int>&map,unordered_map<pair<int,int>,bool>&mp)
+//     {
+//            if(ind==stones.size()-1)
+//                return true;
+//         bool ans1=false, ans2=false, ans3=false;
+//         if(mp.find(make_pair(ind,k))!=mp.end())
+//         {
+//             return mp[make_pair(ind,k)];
+//         }
+//         if(map.find(stones[ind]+k)!=map.end())
+//         {
+//             int curr=map[stones[ind]+k] ;
+//             if(curr>ind)
+//                 ans1=solve(stones,curr,k,map,mp);
+//         }
+//         if(map.find(stones[ind]+k-1)!=map.end())
+//         {
+//             int curr=map[stones[ind]+k-1] ;
+//             if(curr>ind)
+//                 ans2=solve(stones,curr,k-1,map,mp);
+//         }
+//         if(map.find(stones[ind]+k+1)!=map.end())
+//         {
+//             int curr=map[stones[ind]+k+1] ;
+//             if(curr>ind)
+//                 ans3=solve(stones,curr,k+1,map,mp);
+//         }
+        
+//         return mp[make_pair(ind,k)]=ans1||ans2||ans3;
+        
+//     }
+//     bool canCross(vector<int>& stones) {
+//         unordered_map<int, unordered_map<int,bool>> mp;
+//         unordered_map<int,int>map;
+//         for(int i=0;i<stones.size();i++)
+//         {
+//               map[stones[i]]=i;
+//         }
+//         if(stones.size()==2 )
+//         {
+//               if(stones[1]==1)
+//                   return true;
+//             else
+//                  return false;
+//         }
+           
+//         int ind=1;
+//         int k=1;
+      
+//         return solve(stones,ind,k,map,mp);
+//     }
+// };
+class Solution {
+public:
+    bool solve(vector<int>& stones, int ind, int k, unordered_map<int, int>& map, unordered_map<int, unordered_map<int, bool>>& mp) {
+        if (ind == stones.size() - 1)
+            return true;
+        
+        if (mp.find(ind) != mp.end() && mp[ind].find(k) != mp[ind].end()) {
+            return mp[ind][k];
+        }
+        
+        bool ans1 = false, ans2 = false, ans3 = false;
+        if (map.find(stones[ind] + k) != map.end()) {
+            int curr = map[stones[ind] + k];
+            if (curr > ind)
+                ans1 = solve(stones, curr, k, map, mp);
+        }
+        if (k > 1 && map.find(stones[ind] + k - 1) != map.end()) {
+            int curr = map[stones[ind] + k - 1];
+            if (curr > ind)
+                ans2 = solve(stones, curr, k - 1, map, mp);
+        }
+        if (map.find(stones[ind] + k + 1) != map.end()) {
+            int curr = map[stones[ind] + k + 1];
+            if (curr > ind)
+                ans3 = solve(stones, curr, k + 1, map, mp);
+        }
+        
+        return mp[ind][k] = ans1 || ans2 || ans3;
+    }
+    
+    bool canCross(vector<int>& stones) {
+        unordered_map<int, int> map;
+        for (int i = 0; i < stones.size(); i++) {
+            map[stones[i]] = i;
+        }
+        
+        if (stones.size() == 2) {
+            if (stones[1] == 1)
+                return true;
+            else
+                return false;
+        }
+        if(stones[1]!=1)
+            return false;
+        
+        int ind = 1;
+        int k = 1;
+        unordered_map<int, unordered_map<int, bool>> mp;
+        return solve(stones, ind, k, map, mp);
+    }
+};

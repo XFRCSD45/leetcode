0df4e4eab3a67847f5a82b7d8482e7e84d06b20b
Sunny Das <108056620+XFRCSD45@users.noreply.github.com>
2023-10-19 23:58:01 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (15.59%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,57 +1,45 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        int m=s.size(), n=t.size();
-       int del=0, i=m-1;
-        string a="", b="";
-        while(i>=0)
-        {
-             if(s[i]=='#')
-             {
-                 del++;
-                 i--;
-             }
-            else
-            {
-                 if(del>0)
-                 {
-                    del--;
-                     i--;
-                 }
-                else
-                {
-                      a=a+s[i];
-                    i--;
+       int n=s.size()-1;
+        int m=t.size()-1;
+        while(1){
+            int todel=0;
+            while(n>=0 && (todel>0 || s[n]=='#')){
+                if(s[n]=='#'){
+                    todel++;
                 }
-            }
-        }
-        
-        i=n-1;
-        del=0;
-        while(i>=0)
-        {
-             if(t[i]=='#')
-             {
-                 del++;
-                 i--;
-             }
-            else
-            {
-                 if(del>0)
-                 {
-                     del--;
-                     i--;
-                 }
-                else
-                {
-                      b=b+t[i];
-                    i--;
+                else{
+                    todel--;
                 }
+                n--;
             }
+
+            todel=0;
+            while(m>=0 &&(todel>0 || t[m]=='#')){
+                if(t[m]=='#'){
+                    todel++;
+                }
+                else{
+                    todel--;
+                }
+                m--;
+            }
+
+            if(n>=0 && m>=0 && s[n]==t[m]){
+                    n--;
+                    m--;
+            }
+                else{
+                    break;
+                }
+            
         }
-        
-        if(a==b)
-            return true;
-        return false;
+
+
+        if(n==-1 && m==-1){
+            return 1;
+        }
+        return 0;
     }
 };

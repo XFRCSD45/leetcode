029be87eadefb3136245f9ec5268830248a04276
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2025-06-16 15:41:16 +0530

    Time: 0 ms (100%), Space: 8.5 MB (71.9%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if (dividend == INT_MIN && divisor == -1)
-            return INT_MAX;
         bool negative = ( (dividend < 0) ^ (divisor < 0) );
         long long dvd = llabs((long long)dividend);
         long long dvs = llabs((long long)divisor);

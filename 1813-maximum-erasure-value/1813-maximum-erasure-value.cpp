9c35c753706261ab625b9d8b8b705ece2819a7c7
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2025-07-23 02:04:32 +0530

    Time: 15 ms (91.96%), Space: 120.9 MB (50.2%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& v) {
-        int i=0, j=1;
-        int sum=v[i];
+        int i = 0, j = 1;
+        int sum = v[i];
         int n = v.size();
-        int ans=v[0];
-        vector<int>vec(100001,-1);
-        vec[v[i]]=1;
-        while(i<n)
-        {
-            while(j<n)
-            {
-                if(vec[v[j]]==-1)
-                {
-                    sum+=v[j];
-                    vec[v[j]]=1;
-                    j++;
-                }
-                else
-                {
-                    vec[v[i]]=-1;
-                    sum-=v[i];
-                    break;
-                }
-                ans=max(ans,sum);
+        int ans = v[0];
+        vector<int> vec(100001, -1);
+        vec[v[i]] = 1;
+        while (i < n && j < n) {
+
+            if (vec[v[j]] == -1) {
+                sum += v[j];
+                vec[v[j]] = 1;
+                j++;
+            } else {
+                vec[v[i]] = -1;
+                sum -= v[i];
+                i++;
             }
-            i++;
+            ans = max(ans, sum);
         }
         return ans;
     }

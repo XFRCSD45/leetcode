57967df39692301a926d3aec07345ac96b131ae9
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2025-08-02 02:10:37 +0530

    Time: 0 ms (100%), Space: 9.6 MB (88.32%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-           vector<vector<int>>ans;
-       ans.push_back({1});
-            if(numRows==1)
-            {
-
+        vector<vector<int>>ans;
+        ans.push_back({1});
+        if(numRows ==1)
+        {
             return ans;
-            }
-      ans.push_back({1,1});
-        if(numRows==2)
-        {
-              return ans;  
         }
-        for(int i=2;i<numRows;i++)
+        int curr=1;
+        ans.push_back({1,1});
+        for(int i=3;i<=numRows;i++)
         {
-           vector<int>temp(i+1,1);
-            temp[0]=1;
-            for(int j=1;j<i;j++)
-            {
-                 temp[j]=ans[i-1][j-1]+ans[i-1][j];
-            }
-            ans.push_back(temp);
+            vector<int>temp;
+            temp.push_back(1);
+             for(int j=1;j<=i-2;j++)
+             {
+                temp.push_back(ans[curr][j-1]+ans[curr][j]);
+             }
+             temp.push_back(1);
+             ans.push_back(temp);
+             curr++;
         }
         return ans;
     }

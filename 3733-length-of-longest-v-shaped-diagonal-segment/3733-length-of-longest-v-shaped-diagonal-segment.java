05098c50fcfb97cfcb094e32dd21be650ed3d56f
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2025-08-28 01:32:26 +0530

    Time: 1181 ms (16.66%), Space: 138 MB (6.41%) - LeetHub

---
+++ 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.java
@@ -0,0 +1,53 @@
+class Solution {
+    public static int [][] dir = {
+        { 1,1},
+        {1,-1},
+        {-1, -1},
+        {-1,1}
+    };
+    public static int helper(int i, int j,int val, int dr, int n, int m,int f, int [][] grid, int [][][][]dp)
+    {
+        int newRow = i + dir[dr][0];
+        int newCol = j + dir[dr][1];
+        if(newRow <0 || newRow>=n ||newCol<0 || newCol>=m || grid[newRow][newCol]!=val)
+        {
+            return 0;
+        }
+        if(dp[i][j][dr][f]!=0)
+        {
+            return dp[i][j][dr][f];
+        }
+         dp[i][j][dr][f]=1;  
+         int op1 = helper(newRow, newCol,val==2?0:2, dr, n,m,f,grid, dp);
+         int op2 = 0;
+         if(f==1)
+         {
+            op2 = helper(newRow, newCol,val==2?0:2, (dr+1)%4, n,m,0,grid,dp);
+         }
+        dp[i][j][dr][f]+=Math.max(op1, op2);
+        return dp[i][j][dr][f];
+    }
+    public int lenOfVDiagonal(int[][] grid) {
+     int n = grid.length;
+     int m = grid[0].length;
+     int dp [][][][] = new int [n][m][5][2];
+    //  int vis [][][][] = new int [n][m][5][2];
+     int result=0;
+     for(int i=0;i<n;i++)
+     {
+        for(int j=0;j<m;j++)
+        {
+            if(grid[i][j]==1)
+            {
+                System.out.println(i+ " "+j);
+                for(int k=0;k<4;k++)
+                {
+                    result = Math.max(result, 1+ helper(i,j,2,k,n,m,1,grid,dp));
+                    System.out.println(result+ " "+ k);
+                }
+            }
+        }
+     }
+     return result;
+    }
+}

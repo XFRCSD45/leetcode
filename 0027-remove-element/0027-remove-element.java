1afb3318fbd08b52a950d93860f1acec1c70ff0e
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2025-08-30 01:10:19 +0530

    Time: 0 ms (100%), Space: 41.9 MB (68.21%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int pos=0;
+        int n = nums.length;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=val)
+            {
+                nums[pos++]=nums[i];
+            }
+        }
+        return pos;
+    }
+}

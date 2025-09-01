9bf837ad84015a6061770c59e069f0963bf50057
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2025-09-02 01:54:00 +0530

    Time: 293 ms (66.67%), Space: 97.6 MB (88.41%) - LeetHub

--- 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
@@ -1,55 +1,28 @@
 class Solution {
 public:
     double maxAverageRatio(vector<vector<int>>& classes, int extra) {
-        priority_queue<pair<double,int>> pq;
+        priority_queue<pair<double, int>> pq;
         int n = classes.size();
-        int count=0;
-        for(int i=0;i<n;i++)
-        {
+        auto gain = [](int pass, int total) {
+            return (double)(pass + 1) / (total + 1) - (double)pass / total;
+        };
+        for (int i = 0; i < n; i++) {
             int total = classes[i][1];
             int pass = classes[i][0];
-            if(total == pass)
-            {
-                continue;
-            }
-            double avg = pass*1.0/total;
-            pass++;
-            total++;
-            double nexAvg = pass*1.0/total;
-            double change = nexAvg - avg;
-            pq.push({change,i});
+            pq.push({gain(pass, total), i});
         }
-        for(int i=1;i<=extra && !pq.empty();i++)
-        {
-            pair<double,int> p = pq.top();
+        for (int i = 1; i <= extra && !pq.empty(); i++) {
+            auto [change, idx] = pq.top();
             pq.pop();
-            if(p.first ==0.0)
-            {
-                break;
-            }
-            classes[p.second][0]+=1;
-            classes[p.second][1]+=1;
-            int total = classes[p.second][1];
-            int pass = classes[p.second][0];
-            double avg = pass*1.0/total;
-            pass++;
-            total++;
-            double nexAvg = pass*1.0/total;
-            double change = nexAvg - avg;
-            pq.push({change,p.second});
-            // cout<<p.second<<" ";
+            classes[idx][0] += 1;
+            classes[idx][1] += 1;
+            pq.push({gain(classes[idx][0], classes[idx][1]), idx});
         }
-        double sum =0.0;
-        for(int i=0;i<n;i++)
-        {
-            int total = classes[i][1];
-            int pass = classes[i][0];
-            double avg = pass*1.0/total;
-            sum+=avg;
+        double sum = 0.0;
+        for (int i = 0; i < n; i++) {
+            sum += (double)classes[i][0] / classes[i][1];
         }
-        double average = sum/n;
-        average = round(average * 1e5) / 1e5;
+        double average = sum / n;
         return average;
     }
-    
 };

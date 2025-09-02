fec4ea201f1b9221df877d555ced887824b3cfef
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2025-09-03 02:50:11 +0530

    Time: 23 ms (25.36%), Space: 32.7 MB (39.13%) - LeetHub

--- 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.cpp
@@ -7,25 +7,18 @@ public:
             int x = points[i][0];
             int y = points[i][1];
             for (int j = 0; j < n; j++) {
-                if (i == j) {
-                    continue;
-                }
-
                 int x1 = points[j][0];
                 int y1 = points[j][1];
-                if (x1 < x || y1 > y) {
+                if ((i == j) || (x1 < x || y1 > y)) {
                     continue;
                 }
                 int f = 1;
                 for (int k = 0; k < n; k++) {
-                    if (k == i || k == j) {
-                        continue;
-                    }
-                    if (x1 < x || y1 > y) {
-                        continue;
-                    }
                     int x2 = points[k][0];
                     int y2 = points[k][1];
+                    if ((k == i || k == j) || (x1 < x || y1 > y)) {
+                        continue;
+                    }
                     if ((x2 <= x1 && x2 >= x) && (y2 <= y && y2 >= y1)) {
                         f = 0;
                     }

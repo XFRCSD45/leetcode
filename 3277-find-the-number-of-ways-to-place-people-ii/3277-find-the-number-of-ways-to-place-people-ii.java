74c1ba61f774c0d5763b9dfd2951f3e55b832414
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2025-09-04 01:09:48 +0530

    Time: 65 ms (60.9%), Space: 45.3 MB (48.12%) - LeetHub

--- 3277-find-the-number-of-ways-to-place-people-ii/3277-find-the-number-of-ways-to-place-people-ii.java
+++ 3277-find-the-number-of-ways-to-place-people-ii/3277-find-the-number-of-ways-to-place-people-ii.java
@@ -8,40 +8,25 @@ class Solution {
                 return Integer.compare(b[1], a[1]);
             }
         });
-        int ans=0;
+        int ans = 0;
 
-        for(int i=0;i<n;i++)
-        {
+        for (int i = 0; i < n; i++) {
             int x = points[i][0];
             int y = points[i][1];
             int largestY = Integer.MIN_VALUE;
-            int smallestX = Integer.MAX_VALUE;
-            int found =0;
-            int curr=0;
-            for(int j=i+1;j<n;j++)
-            {
+            for (int j = i + 1; j < n; j++) {
                 int x1 = points[j][0];
                 int y1 = points[j][1];
-                if(y1<=y)
-                {
-                    if(found == 0)
-                    {
-                        smallestX = x1;
-                        largestY = y1;
-                        curr++;
-                        found=1;
-                    }
-                    else if(x1 > smallestX && y1 > largestY)
-                    {
-                        smallestX = x1;
-                        largestY = y1;
-                        curr++;
-                        found=1;
-                    }
+                if (y1 > y) {
+                    continue;
                 }
+
+                if (y1 > largestY) {
+                    ans++;
+                    largestY = y1;
+                }
+
             }
-            // System.out.println(curr);
-            ans+=curr;
         }
         return ans;
     }

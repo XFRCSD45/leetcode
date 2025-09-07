001435c03f4dd2d70298238e00bb3a1252ed3ad3
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2025-09-07 07:24:37 +0530

    Time: 3 ms (88.66%), Space: 25.9 MB (24.01%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -8,117 +8,68 @@ public:
         }
         return true;
     }
-    pair<int, int> dfs(pair<int, int> p, pair<int, int> pos,
-                       vector<vector<int>>& heights, vector<vector<int>>& vis,
-                       vector<vector<pair<int, int>>>& ans) {
-        int x = pos.first;
-        int y = pos.second;
-        vis[x][y] = 1;
-
-        if ((x == 0 && y == cols - 1) || (x == rows - 1 && y == 0)) {
-            return {1, 1};
-        }
-        pair<int, int> res = ans[x][y];
-        int currHeight = heights[x][y];
-        vector<pair<int,int>>equals;
-        for (auto i : dir) {
-            int newX = x + i.first;
-            int newY = y + i.second;
-
-            if (isValid(newX, newY) && !(newX == p.first && newY == p.second) && heights[newX][newY] <= currHeight) {
-                //  cout<<x<<" "<<y<<" "<<newX<<" "<<newY<<endl;
-                if(heights[newX][newY]==currHeight)
-                {
-                    equals.push_back({newX,newY});
-                }
-                pair<int, int> response;
-                if(vis[newX][newY])
-                {
-                    response = ans[newX][newY];
-                }
-                else
-                {
-                    response =
-                    dfs(pos, {newX, newY}, heights, vis, ans);
-                }
-                ans[x][y] = {ans[x][y].first || response.first,
-                             ans[x][y].second || response.second};
-            }
-        }
-        for(auto i : equals)
-        {
-            ans[i.first][i.second]= {ans[i.first][i.second].first ||ans[x][y].first, ans[i.first][i.second].second ||ans[x][y].second};
-        }
-        return ans[x][y];
-    }
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         int n = heights.size();
         int m = heights[0].size();
-        vector<vector<int>> result;
-        if(n==1 || m==1)
-        {
-            for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                    result.push_back({i, j});
-            }
-        }
-            return result;
-        }
         rows = n;
         cols = m;
         vector<vector<int>> vis(n, vector<int>(m, 0));
-        vector<vector<pair<int, int>>> ans(n, vector<pair<int, int>>(m));
+        vector<vector<int>> pac(n, vector<int>(m, 0));
+        vector<vector<int>> atl(n, vector<int>(m, 0));
+        queue<pair<int,int>> q;
         for (int i = 0; i < n; i++) {
-            ans[i][0] = {1, 0};
-            ans[i][m - 1] = {0, 1};
+            q.push({i, 0});
+            pac[i][0] = 1;
         }
         for (int j = 0; j < m; j++) {
-            ans[0][j] = {1, 0};
-            ans[n - 1][j] = {0, 1};
+            q.push({0, j});
+            pac[0][j] = 1;
         }
-        ans[0][m - 1] = {1, 1};
-        ans[n - 1][0] = {1, 1};
+        while (!q.empty()) {
+                        pair<int, int> p = q.front();
+                        q.pop();
+                        vis[p.first][p.second] = 1;
+                        for (auto d : dir) {
+                            int x = p.first + d.first;
+                            int y = p.second + d.second;
+                            if (isValid(x, y) && !vis[x][y] &&
+                                heights[x][y] >= heights[p.first][p.second]) {
+                                pac[x][y] = 1;
+                                q.push({x, y});
+                            }
+                        }
+                    }
+        vis.assign(n, vector<int>(m, 0));
+        for (int i = 0; i < n; i++) {
+            q.push({i, m-1});
+            atl[i][m-1] = 1;
+        }
+        for (int j = 0; j < m; j++) {
+            q.push({n-1, j});
+            atl[n-1][j] = 1;
+        }
+        while (!q.empty()) {
+                        pair<int, int> p = q.front();
+                        q.pop();
+                        vis[p.first][p.second] = 1;
+                        for (auto d : dir) {
+                            int x = p.first + d.first;
+                            int y = p.second + d.second;
+                            if (isValid(x, y) && !vis[x][y] &&
+                                heights[x][y] >= heights[p.first][p.second]) {
+                                atl[x][y] = 1;
+                                q.push({x, y});
+                            }
+                        }
+                    }
+        vector<vector<int>> ans;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                 if(!vis[i][j])
-                {
-                    dfs({-1, -1}, {i, j}, heights, vis, ans);
+                if (pac[i][j] && atl[i][j]) {
+                    ans.push_back({i, j});
                 }
             }
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if((i==0 || j==0 || i==n-1||j==m-1 ))
-                {
-                    dfs({-1, -1}, {i, j}, heights, vis, ans);
-                }
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                vis[i][j]=0;
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {               
-                    dfs({-1, -1}, {i, j}, heights, vis, ans);
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if((i==0 || j==0 || i==n-1||j==m-1 ))
-                {
-                    dfs({-1, -1}, {i, j}, heights, vis, ans);
-                }
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (ans[i][j].first == 1 && ans[i][j].second == 1) {
-                    result.push_back({i, j});
-                }
-            }
-        }
-        return result;
+        return ans;
     }
 };

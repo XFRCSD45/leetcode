c9d7ce273910bf5dd32b5cc5bdcfc42c7d24afed
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2025-10-05 10:52:16 +0530

    Time: 0 ms (100%), Space: 21.8 MB (70.2%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,8 +2,25 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-       int sum = (n*(n+1))/2;
-       int total = accumulate(nums.begin(), nums.end(),0);
-       return sum-total;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < n && nums[i] != -1) {
+                int j = nums[i];
+                while (nums[j] != -1) {
+                    if (nums[j] >= n) {
+                        nums[j] = -1;
+                        break;
+                    }
+                    int temp = nums[j];
+                    nums[j] = -1;
+                    j = temp;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != -1) {
+                return i;
+            }
+        }
+        return n;
     }
 };

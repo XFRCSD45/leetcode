a6039c31a89f1d9917c17492dff1861715cb3d6f
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2025-10-15 23:08:51 +0530

    Time: 320 ms (9.02%), Space: 291.7 MB (5.26%) - LeetHub

---
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool hasIncreasingSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int>prefix(n,0);
+        prefix[0]=1;
+        for(int i=1;i<n;i++)
+        {
+             if(nums[i-1]<nums[i])
+             {
+                prefix[i]=prefix[i-1]+1;
+             }
+             else
+             {
+                prefix[i]=1;
+             }
+        }
+        for(int i=k-1;i<n-k;i++)
+        {
+             if(prefix[i]>=k && prefix[i+k]>=k )
+             {
+                return true;
+             }
+        }
+        return false;
+    }
+    int maxIncreasingSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        int low=1;
+        int high = n;
+        int ans =INT_MIN;
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            if(hasIncreasingSubarrays(nums,mid))
+            {
+                ans = max(ans,mid);
+                low=mid+1;
+            }
+            else
+            {
+                high = mid-1;
+            }
+        }
+     return ans;   
+    }
+};

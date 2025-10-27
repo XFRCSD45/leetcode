8095e4caeb265bf5d22a9d2e006f6c864b25b8e9
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2025-10-28 01:35:43 +0530

    Time: 5 ms (66.16%), Space: 27.5 MB (54.2%) - LeetHub

--- 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cpp
@@ -1,28 +1,12 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        int n = bank.size();
-        vector<pair<int, int>> cams(n);
-        int m = bank[0].size();
-        int prev = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            int curr = 0;
-            for (int j = 0; j < m; j++) {
-                if (bank[i][j] == '1') {
-                    curr++;
-                }
+        int ans=0, prev=0;
+        for(auto& row: bank){
+            int device=count(row.begin(), row.end(), '1');
+            if (device>0){
+                ans+=exchange(prev,device)*device;
             }
-            if (curr == 0) {
-                cams[i] = {0, prev};
-            } else {
-                cams[i] = {curr, prev};
-                prev = curr;
-            }
-        }
-        int ans=0;
-        for(int i=n-2;i>=0;i--)
-        {
-            ans+=cams[i].first*cams[i].second;
         }
         return ans;
     }

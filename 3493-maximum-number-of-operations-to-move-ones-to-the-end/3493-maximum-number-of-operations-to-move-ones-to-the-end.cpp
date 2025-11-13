f3d0c65aa991f31bb8b2cebe77a81e97fbc7f23d
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2025-11-14 03:14:41 +0530

    Time: 7 ms (43.07%), Space: 15.9 MB (54.38%) - LeetHub

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     int maxOperations(string s) {
-        int n = s.size();
-        vector<int>prefix(n,0);
-        int ans=0;
-        for(int i=0;i<n;i++)
-        {
-            prefix[i]+=s[i]=='1'?1:0;
-            if(i>0)
-            {
-                prefix[i]+=prefix[i-1];
-            }
-        }
-        for(int i=n-1;i>=0;i--)
-        {
-            if(s[i]=='0')
-            {
-                ans+=prefix[i];
-            }
-            while(i>=0&&s[i]=='0')
-            {
-                i--;
+        int countOne = 0;
+        int ans = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '0') {
+                while ((i + 1) < s.length() && s[i + 1] == '0') {
+                    i++;
+                }
+                ans += countOne;
+            } else {
+                countOne++;
             }
         }
         return ans;

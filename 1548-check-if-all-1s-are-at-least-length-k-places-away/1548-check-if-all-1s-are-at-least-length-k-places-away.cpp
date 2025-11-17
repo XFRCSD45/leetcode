9b05fc56b4b65a9cb3283f01cbbc3cb3ac7780ae
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2025-11-17 23:47:20 +0530

    Time: 0 ms (100%), Space: 61.5 MB (47.64%) - LeetHub

---
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+        int last =-1;
+        int n = nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==1)
+            {
+               if(last!=-1 && (i-last-1)<k)
+               {
+                  return false;
+               }
+               last =i;
+            }
+        }
+        return true;
+    }
+};

937f39f4e3b458de80eadca24af555d050572065
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2026-01-07 23:17:00 +0530

    Time: 3 ms (74.88%), Space: 80.1 MB (22.77%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -1,47 +1,34 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-int mod = 1000000007;
-int totalSum =0;
-long long ans=0;
-    long long findSum(TreeNode* root)
-    {
-        if(!root)
-        {
-            return 0;
-        }
-        long long leftSum = findSum(root->left);
-        long long rightSum = findSum(root->right);
-        long long total = ((leftSum + rightSum) + root->val);
-        return total;
+    long long MOD = 1e9 + 7;
+    long long ans = 0;
+
+    long long dfs(TreeNode* node) {
+        if (!node) return 0;
+
+        node->val += dfs(node->left) + dfs(node->right);
+        return node->val;
     }
-    long long solve(TreeNode* root)
-    {
-        if(root == nullptr)
-        {
-            return 0;
-        }
-        long long leftSum = solve(root->left);
-        long long rightSum = solve(root->right);
-        long long op1= ((totalSum-leftSum)*1LL*leftSum);
-        long long op2 = ((totalSum-rightSum)*1LL*rightSum);
-        ans=max(ans,max(op1,op2));
-        long long total = ((leftSum + rightSum) + root->val);
-        return total;
-    }
+
     int maxProduct(TreeNode* root) {
-    totalSum = findSum(root);
-    solve(root);
-    return (int)(ans%mod);   
+        long long total = dfs(root);
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            if (!node) continue;
+
+            long long cur = (total - node->val) * node->val;
+            ans = max(ans, cur);
+
+            if (node->left) q.push(node->left);
+            if (node->right) q.push(node->right);
+        }
+
+        return ans % MOD;
     }
 };

3da052031eb275f1dbe8519004af4c88f7651ceb
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2026-01-15 21:45:57 +0530

    Time: 4 ms (3.48%), Space: 31.8 MB (99.13%) - LeetHub

---
+++ 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int maximizeSquareHoleArea(int n, int m, vector<int>& hBars, vector<int>& vBars) {
+        sort(vBars.begin(), vBars.end());
+        sort(hBars.begin(), hBars.end());
+        int hMaxi=1, vMaxi=1;
+        int hCurr=1, vCurr=1;
+        for(int i=1;i<hBars.size();i++)
+        {
+            if((hBars[i]-hBars[i-1])==1)
+            {
+                hCurr++;
+                hMaxi=max(hMaxi,hCurr);
+            }
+            else
+            {
+                hCurr=1;
+            }
+        }
+        for(int i=1;i<vBars.size();i++)
+        {
+            if((vBars[i]-vBars[i-1])==1)
+            {
+                vCurr++;
+                vMaxi=max(vMaxi,vCurr);
+            }
+            else
+            {
+                vCurr=1;
+            }
+        }
+        long long  ans = 1LL* (min(hMaxi,vMaxi)+1LL)*(min(hMaxi,vMaxi)+ 1LL);
+        return (int)ans;
+    }
+};

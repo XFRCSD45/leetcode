65e171cde11f3560c8dd2c61a697897dc6f15d68
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2026-02-25 21:25:26 +0530

    Time: 32 ms (20.78%), Space: 29 MB (21.26%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& s) {
-         unordered_map<string, vector<string>> mp;
-        
-        for(auto x: strs){
-            string word = x;
-            sort(word.begin(), word.end());
-            mp[word].push_back(x);
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>>ans;
+        unordered_map<string,vector<string>>m;
+        int n= strs.size();
+        for(int i=0;i<n;i++)
+        {
+            string temp(26,'0');
+            for(int j=0;j<strs[i].size();j++)
+            {
+                int curr = strs[i][j] -'a';
+                int tempCurr=temp[curr]-'0';
+                tempCurr++;
+                temp[curr]=tempCurr+'0';
+            }
+            m[temp].push_back(strs[i]);
         }
-        
-        vector<vector<string>> ans;
-        for(auto x: mp){
-            ans.push_back(x.second);
+        for(auto i:m)
+        {
+            ans.push_back(i.second);
         }
         return ans;
-        
     }
 };

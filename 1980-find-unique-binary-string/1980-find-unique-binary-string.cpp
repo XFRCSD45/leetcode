5fe5941e1b38fc747d5e6cec2909c5a48f52c1d3
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2026-03-08 13:39:14 +0530

    Time: 0 ms (100%), Space: 12.8 MB (55.15%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     string findDifferentBinaryString(vector<string>& nums) {
-        int n = nums[0].size();
-        unordered_map<string,bool>m;
-        for(int i=0;i<nums.size();i++)
-        {
-            m[nums[i]]=true;
+        string ans;
+        for (int i = 0; i < nums.size(); i++) {
+            char curr = nums[i][i];
+            ans += curr == '0' ? '1' : '0';
         }
-        for (int i = 0; i < (1 << n); i++) {
-        string s;
-         for (int j = n - 1; j >= 0; --j)
-            s += ((i >> j) & 1) ? '1' : '0';
-        if(!m[s])
-        {
-            return s;
-        }
-        }
-        return "";
+        
+        return ans;
     }
 };

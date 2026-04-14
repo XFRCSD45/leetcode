2794a776815126ced29cbc5c07bde20aa89ab7d5
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2026-04-15 03:12:24 +0530

    Time: 23 ms (69.59%), Space: 13.5 MB (75%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.cpp
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.cpp
@@ -8,28 +8,26 @@ public:
         int m = factory.size();
 
         const long long INF = 1e18;
-        vector<vector<vector<long long>>> dp(n + 1, vector<vector<long long>>(m + 1, vector<long long>(101,INF)));
-        function<long long(int, int, int)> solve = [&](int i, int j, int count)-> long long{
-            if(i>=n)
-            {
-                return 0;
+        vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, INF));
+
+        for (int j = 0; j <= m; j++)
+            dp[0][j] = 0;
+
+        for (int j = 1; j <= m; j++) {
+            int pos = factory[j-1][0];
+            int limit = factory[j-1][1];
+
+            for (int i = 0; i <= n; i++) {
+                dp[i][j] = dp[i][j-1];
+
+                long long dist = 0;
+                for (int k = 1; k <= limit && i - k >= 0; k++) {
+                    dist += abs(robot[i-k] - pos);
+                    dp[i][j] = min(dp[i][j], dp[i-k][j-1] + dist);
+                }
             }
-            if(j>=m)
-            {
-                return INF;
-            }
-            if(dp[i][j][count]!=INF)
-            {
-                return dp[i][j][count];
-            }
-            long long option1 = solve(i, j+1, 0);
-            long long option2 = INF;
-            if(count< factory[j][1])
-            {
-                option2 = abs(robot[i]-factory[j][0]) + min(solve(i+1, j,count+1), solve(i+1, j+1, 0));
-            }
-            return dp[i][j][count]=min(option1, option2);
-        };
-        return solve(0,0,0);
+        }
+
+        return dp[n][m];
     }
 };

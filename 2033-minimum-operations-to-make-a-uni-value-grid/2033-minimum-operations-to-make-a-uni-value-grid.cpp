fedadc26a37d0beac33d92bca4dd2f37d4a7720c
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2026-04-28 18:46:29 +0530

    Time: 53 ms (17.7%), Space: 87 MB (78.6%) - LeetHub

---
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int solve(vector<int>& v, int x, int pos) {
+        int n = v.size();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int diff = abs(v[pos] - v[i]) / x;
+            ans += diff;
+        }
+        return ans;
+    }
+    int minOperations(vector<vector<int>>& grid, int x) {
+        vector<int> vec;
+        int mod = -1;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                vec.push_back(grid[i][j]);
+                if (mod != -1 && grid[i][j] % x != mod) {
+                    return -1;
+                } else {
+                    mod = grid[i][j] % x;
+                }
+            }
+        }
+        sort(vec.begin(), vec.end());
+        int ans = max(solve(vec, x, (vec.size()) / 2),
+                      solve(vec, x, (vec.size()- 1) / 2));
+        return ans;
+    }
+};

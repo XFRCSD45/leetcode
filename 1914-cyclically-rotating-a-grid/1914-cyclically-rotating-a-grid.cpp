ba3d526af608b096cb199fefb0f5bd674cd3a113
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2026-05-10 03:24:18 +0530

    Time: 16 ms (45.99%), Space: 17.8 MB (42.34%) - LeetHub

---
+++ 1914-cyclically-rotating-a-grid/1914-cyclically-rotating-a-grid.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    vector<vector<int>> rotateGrid(vector<vector<int>>& grid, int k) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        int layers = min(m, n) / 2; 
+
+        for(int layer = 0; layer < layers; layer++) {
+            vector<int> nums;
+
+            int top = layer;
+            int bottom = n - layer -1;
+            int left = layer;
+            int right = m - layer - 1;
+
+            for(int j = left; j <= right; j++) {
+                nums.push_back(grid[top][j]);
+            }
+
+            for(int i = top+1; i <= bottom-1; i++) {
+                nums.push_back(grid[i][right]);
+            }
+
+            for(int j = right; j >= left; j--) {
+                nums.push_back(grid[bottom][j]);
+            }
+
+            for(int i = bottom-1; i >= top+1; i--) {
+                nums.push_back(grid[i][left]);
+            }
+
+            int len = nums.size();
+
+            int nomralized_k = k % len;
+
+            rotate(nums.begin(), nums.begin() + nomralized_k, nums.end());
+
+            int idx = 0;
+
+            for(int j = left; j <= right; j++) {
+                grid[top][j] = nums[idx++];
+            }
+
+            for(int i = top+1; i <= bottom-1; i++) {
+                grid[i][right] = nums[idx++];
+            }
+
+            for(int j = right; j >= left; j--) {
+                grid[bottom][j] = nums[idx++];
+            }
+
+            for(int i = bottom-1; i >= top+1; i--) {
+                grid[i][left] = nums[idx++];
+            }
+        }
+
+        return grid;
+    }
+};

741d8d1c720c2f714530b929425b682088e122a4
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2026-05-13 00:46:30 +0530

    Time: 91 ms (23.3%), Space: 110.4 MB (26.21%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -6,30 +6,13 @@ public:
                  int diffA = abs(a[0] - a[1]);
                  int diffB = abs(b[0] - b[1]);
                  if (diffA != diffB)
-                     return diffA > diffB;
+                     return diffA < diffB;
                  return a[1] < b[1];
              });
 
-        int low = 0, high = INT_MAX;
-        int ans = INT_MAX;
-        function<bool(int)> check = [&](int K) -> bool {
-            int curr = K;
-            for (int i = 0; i < tasks.size(); i++) {
-                if (curr < tasks[i][1]) {
-                    return false;
-                }
-                curr -= tasks[i][0];
-            }
-            return true;
-        };
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (check(mid)) {
-                ans = min(ans, mid);
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
+        int ans = 0;
+        for (auto task : tasks) {
+            ans = max(ans + task[0], task[1]);
         }
         return ans;
     }

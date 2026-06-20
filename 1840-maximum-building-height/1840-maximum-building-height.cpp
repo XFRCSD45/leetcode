ffdb0f5d9108bc1ec2a3b0f0c39c520d27fff75d
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2026-06-21 02:08:31 +0530

    Time: 87 ms (36.99%), Space: 106.7 MB (58.9%) - LeetHub

---
+++ 1840-maximum-building-height/1840-maximum-building-height.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int maxBuilding(int n, vector<vector<int>>& restrictions) {
+        restrictions.push_back({1, 0});
+
+        sort(restrictions.begin(), restrictions.end());
+
+        int k = restrictions.size();
+
+        for (int i = 1; i < k; i++) {
+            int dist = restrictions[i][0] - restrictions[i - 1][0];
+            restrictions[i][1] = min(restrictions[i][1],
+                                     restrictions[i - 1][1] + dist);
+        }
+
+        for (int i = k - 2; i >= 0; i--) {
+            int dist = restrictions[i + 1][0] - restrictions[i][0];
+            restrictions[i][1] = min(restrictions[i][1],
+                                     restrictions[i + 1][1] + dist);
+        }
+
+        int ans = 0;
+
+        for (int i = 1; i < k; i++) {
+            int id1 = restrictions[i - 1][0];
+            int h1 = restrictions[i - 1][1];
+
+            int id2 = restrictions[i][0];
+            int h2 = restrictions[i][1];
+
+            int dist = id2 - id1;
+
+            int peak = (h1 + h2 + dist) / 2;
+
+            ans = max(ans, peak);
+        }
+
+        int lastId = restrictions[k - 1][0];
+        int lastHeight = restrictions[k - 1][1];
+
+        ans = max(ans, lastHeight + (n - lastId));
+
+        return ans;
+    }
+};

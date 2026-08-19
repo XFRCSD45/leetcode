9a45f8f7011cbce1974ac7dd1688de8c3c8933ea
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2026-08-20 02:33:34 +0530

    Time: 46 ms (51.75%), Space: 65.8 MB (48.92%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -1,42 +1,29 @@
 class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
-        int m = reservedSeats.size();
-        int ans = 0;
-        sort(reservedSeats.begin(), reservedSeats.end(),
-             [&](const vector<int>& a, const vector<int>& b) {
-                 if (a[0] == b[0]) {
-                     return a[1] < b[1];
-                 }
-                 return a[0] < b[0];
-             });
-        int rows = 0;
-        int i = 0;
-        while (i < m) {
-            int currRow = reservedSeats[i][0];
-            int j = i;
-            int first = 1, second = 1, third = 1;
-            while (j < m && reservedSeats[j][0] == reservedSeats[i][0]) {
-                if (reservedSeats[j][1] >= 2 && reservedSeats[j][1] <= 5) {
-                    first = 0;
-                }
-                if (reservedSeats[j][1] >= 4 && reservedSeats[j][1] <= 7) {
-                    second = 0;
-                }
-                if (reservedSeats[j][1] >= 6 && reservedSeats[j][1] <= 9) {
-                    third = 0;
-                }
-                j++;
+        unordered_map<int, int> mp;
+        for(auto it : reservedSeats){
+            int row = it[0];
+            int seatNo = it[1];
+            if(seatNo >= 2 && seatNo <= 9){
+                mp[row] |= (1 << (seatNo - 2));
             }
-            if (first && third) {
-                ans += 2;
-            } else if (first || second || third) {
-                ans += 1;
-            }
-            i = j;
-            rows++;
         }
-        ans = ans + (n - rows) * 2;
-        return ans;
+        int sol = (n - mp.size()) * 2;
+        int leftMask = 0b00001111;
+        int midMask = 0b00111100;
+        int rightMask = 0b11110000;
+        for(auto it : mp){
+            int currMask = it.second;
+            bool left = ((currMask & leftMask) == 0);
+            bool mid = ((currMask & midMask) == 0);
+            bool right = ((currMask & rightMask) == 0);
+            if(left && right){
+                sol += 2;
+            }else if(left || mid || right){
+                sol++;
+            }
+        }
+        return sol;
     }
 };

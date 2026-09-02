6777db924be7e4a5e9dcfd42b7a90d782335999a
XFRCSD45 <108056620+XFRCSD45@users.noreply.github.com>
2026-09-02 16:28:42 +0530

    Time: 0 ms (100%), Space: 97.4 MB (61.41%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini = prices[0];
+        int ans=0;
+        int n = prices.size();
+        for(int i=0;i<n;i++)
+        {
+            mini=min(mini, prices[i]);
+            ans=max(ans, prices[i]-mini);
+        }
+        return ans;
+    }
+};
